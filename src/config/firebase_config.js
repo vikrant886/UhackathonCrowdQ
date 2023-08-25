bb664655f0baf2bb0182eb825f9739a52dace7b2
Vikrant rana <vikrantrana817@gmail.com>
2023-08-25 11:05:18 +0530

    Update firebase_config.js

--- src/config/firebase_config.js
+++ src/config/firebase_config.js
@@ -5,11 +5,7 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore'
 import { getStorage } from "firebase/storage";
 import { isExportDeclaration } from "typescript";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAphJJN2v0lPoCHiCqOsM2nEQpBaZ0-IPQ",
   authDomain: "crowd-detection-4a9f3.firebaseapp.com",
